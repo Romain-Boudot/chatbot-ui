db80084c404e48e4160123dc3ac7d93c0eac3c61
Romain Boudot <boudotromain@gmail.com>
2023-06-19 00:34:20 +0200

    working ...

--- types/openai.ts
+++ types/openai.ts
@@ -1,5 +1,3 @@
-import { OPENAI_API_TYPE } from '../utils/app/const';
-
 export interface OpenAIModel {
   id: string;
   name: string;
@@ -8,38 +6,17 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
+  Wizard7B = '/models/WizardLM-7B-uncensored.ggmlv3.q5_0.bin',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.Wizard7B;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
+  [OpenAIModelID.Wizard7B]: {
+    id: OpenAIModelID.Wizard7B,
+    name: 'Wizard 7B',
     maxLength: 12000,
     tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
+  }
 };
